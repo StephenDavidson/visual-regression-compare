7567b8ceced19dbd1e25700a50143e016fbf13f5
Stephen Davidson <stephend.qa@gmail.com>
2021-07-23 15:36:29 +0200

    update docker image version playwright

--- cypress/cypress/integration/visual.spec.ts
+++ cypress/cypress/integration/visual.spec.ts
@@ -9,6 +9,7 @@ const baseUrl = 'https://playwright.dev'
 describe('Visual Regression', () => {
   for (const path of urls) {
     it(`Visual regression - ${path}`, () => {
+      cy.viewport(1280, 1162);
       cy.visit(`${baseUrl}/${path}`)
       cy.document().matchImageSnapshot();
     })
